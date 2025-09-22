b76fd1f295a606fd418be09604369528c9b01ba3
Triyaambak S <triyaambaks2003@gmail.com>
2025-09-22 22:31:02 +0530

    configuring daemon for namenode

--- api/daemon/namenode/namenode.go
+++ api/daemon/namenode/namenode.go
@@ -1,2 +1,10 @@
 // Package namenode is to define and declare all functionalities of the namenode server
 package namenode
+
+import "fmt"
+
+type Namenode struct{}
+
+func InitNamenode() {
+	fmt.Println("Initializing namenode")
+}
